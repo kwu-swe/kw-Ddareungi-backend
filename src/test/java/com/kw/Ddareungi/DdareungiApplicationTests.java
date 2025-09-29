c1faa37b291b465277016c8da6a895ed70d3c65a
hann <wjdgks1233@gmail.com>
2025-09-29 22:26:27 +0900

    init commit

---
+++ src/test/java/com/kw/Ddareungi/DdareungiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kw.Ddareungi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdareungiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
